734abcdf255c4f6d2b8217d4e8dd7b6be86b08a7
Nabeel Muhammad <nabeel.mbab@gmail.com>
2024-07-20 11:57:55 +0800

    added link to portfolio site

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
     <footer className="footer footer-center bg-base-300 text-base-content p-4 bottom-0 absolute">
       <aside>
-        <p>Copyright © Nabeel Muhammad {new Date().getFullYear()} </p>
+        <p>Copyright © <Link target="_blank" href="https://nabeel-mbab.netlify.app">Nabeel Muhammad </Link>{new Date().getFullYear()} </p>
       </aside>
     </footer>
   );
